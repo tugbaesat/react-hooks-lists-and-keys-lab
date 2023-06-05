f585f1a72434471da5a80f2e0b32dbc7603c83f4
Tugba Esat <tugbaesat007@gmail.com>
2023-06-05 21:15:19 +0300

    completed assignment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,12 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const navElements = links.map((link )=> {
+    const linkText = link.toUpperCase();
+    return <a key={link} href={`#${link}`}>{linkText}</a>
+  })
+
+  return <nav>{navElements}</nav>;
 }
 
 export default NavBar;
